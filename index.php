b6939c04e0f9ccbb7e16525cf6c3edc687f8c05a
Mao Yamamura <myamamura@myamamura01.local>
2017-10-23 00:10:08 +0900

    3章テキスト返信

--- index.php
+++ index.php
@@ -8,10 +8,9 @@
   // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
   $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_ACCESS_TOKEN')]);
   // LINE Messaging APIがリクエストに付与した署名を取得
-  $signature = $_SERVER['HTTP' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+  $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
   // 署名が正当かチェック。正当であればリクエストをパースし配列へ
-  $events = $bot->parseEventRequest(file_get_contents('php://input'),
-      $signature);
+  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
       // 配列の格納された各イベントをループで処理
       foreach ($envents as $event) {
         // テキストを返信
