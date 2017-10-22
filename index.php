bde24d4371d30af5a5314ca02ad2ad27743f4020
Mao Yamamura <myamamura@myamamura01.local>
2017-10-22 22:34:25 +0900

    typoしてたため修正

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Composerでインストールしたライブラリを一括読み込み
-  require_once __DIR__ . '/vender/autoload.php';
+  require_once __DIR__ . '/vendor/autoload.php';
 // POSTメソッドで渡される値を取得、表示
   $inputString = file_get_contents('php://input');
   error_log($inputString);
