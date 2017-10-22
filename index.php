37e1878ba8d5ffd750a675501c25ba39a55ce66b
Mao Yamamura <myamamura@myamamura01.local>
2017-10-23 00:12:26 +0900

    3章テキスト返信

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
   require_once __DIR__ . '/vendor/autoload.php';
 
 // アクセストークンを使いCurlHTTPClientをインスタンス化
-  $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_SECRET'));
+  $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
   // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
-  $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_ACCESS_TOKEN')]);
+  $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
   // LINE Messaging APIがリクエストに付与した署名を取得
   $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
   // 署名が正当かチェック。正当であればリクエストをパースし配列へ
