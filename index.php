865a53c612d9cea0457e4e2dac76df13965637a9
Mao Yamamura <myamamura@myamamura01.local>
2017-10-23 00:03:29 +0900

    3章テキスト返信

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
   require_once __DIR__ . '/vendor/autoload.php';
 
 // アクセストークンを使いCurlHTTPClientをインスタンス化
-  $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('kGxlKnqE9XpSljsTSn55fiS4CSzb90Q5J8Qb9YifkcRtWokFiPur08HYIdVlwXfhhMkLufiQn3nQzTO7J0h4/zzuEBmUHL8iSn38LsPkWzK0GD5dtag7OwVjLmva7atXUdz83MYniy9VzCPoJ1JMrWDNKEV4tduUv0NICtwdB04t89/1O/w1cDnyilFU='));
+  $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_SECRET'));
   // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
-  $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getnev('22bf2f05131002dab13c5afd8f0c15fc')]);
+  $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getnev('CHANNEL_ACCESS_TOKEN')]);
   // LINE Messaging APIがリクエストに付与した署名を取得
-  $singnature = $_SERVER['HTTP' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+  $signature = $_SERVER['HTTP' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
   // 署名が正当かチェック。正当であればリクエストをパースし配列へ
   $events = $bot->parseEventRequst(file_get_contents('php://input'),
-      $singnature);
+      $signature);
       // 配列の格納された各イベントをループで処理
       foreach ($envents as $event) {
         // テキストを返信
